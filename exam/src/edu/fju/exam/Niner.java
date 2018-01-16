30f5c0bb3a3f86d742bbce8256d5837027757a75
hshken <hshken@livemail.tw>
2018-01-16 14:31:19 +0800

    exam

---
+++ exam/src/edu/fju/exam/Niner.java
@@ -0,0 +1,19 @@
+package edu.fju.exam;
+
+public class Niner {
+
+	public static void main(String[] args) {
+		for (int n = 2 ; n<=5 ; n++){
+			for (int i = 1 ; i <= 5 ; i++){
+				if (i==5){
+					System.out.println();
+				}
+				System.out.print(n + "x" + i+ "="+n * i + "\t");
+				
+				}
+			}
+		}
+
+	}
+
+
