8f78a1d97363b8dce4b04f273e36d49ac5daba94
hshken <hshken@livemail.tw>
2018-01-16 15:20:27 +0800

    exam

---
+++ exam/src/edu/fju/exam/Employee.java
@@ -0,0 +1,15 @@
+package edu.fju.exam;
+
+public class Employee {
+  int wage;
+  int bonus;
+  String name;
+  public Employee(){
+  }
+  public Employee(String name , int wage){
+	  this.name = name;
+	  this.wage = wage;
+	  
+  }
+}
+
